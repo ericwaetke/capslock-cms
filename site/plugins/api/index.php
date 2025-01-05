01d3ebdf5e246c1f72e75b3b7115fa0fd78a823a
Eric Wätke <eric.waetke@fh-potsdam.de>
2025-01-05 16:13:18 +0100

    chore: Auth wip

--- site/plugins/api/index.php
+++ site/plugins/api/index.php
@@ -2,13 +2,39 @@
 
 Kirby::plugin('ericwaetke/api', [
   'api' => [
-    'routes' => [
-      [
-        'pattern' => 'csrf',
-        'action'  => function () {
-          return csrf();
-        },
-      ]
-    ]
+    'routes' => function($kirby) {
+      return [
+        [
+          'pattern' => 'customLogin',
+          'action'  => function () use ($kirby) {
+            if($username = get('email') and $password = get('password')) {
+
+              $auth = $kirby->auth();
+              $user = $auth->login($username, $password);
+            
+              if($user) {
+                // Return JSON user and csrf token
+                $csrf = $auth->csrfFromSession();
+                $user = $user->toArray();
+                $user['csrf'] = $csrf;
+                $user['status'] = $auth->log();
+                return $user;
+              } else {
+                return 'invalid username or password';
+              }
+            
+            } else {
+              if (!$username) {
+                return 'missing username';
+              }
+              if (!$password) {
+                return 'missing password';
+              }
+            }
+            // return csrf();
+          },
+        ]
+        ];
+    } 
   ]
 ]);
